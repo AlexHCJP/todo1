92d8728dcf82d44fb2e03e7d436a142a47d01513
Alexander Bangert <alexganbert@gmail.com>
2024-09-25 20:29:22 +0500

    render form

--- scripts/sctips.js
+++ scripts/sctips.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('data');
+    const Form = () => {
+        const formInput = document.querySelector('#form-input');
+        console.log(formInput);
+
+        const inputElement = document.createElement('input');
+        inputElement.setAttribute('type', 'text');
+        inputElement.className = 'form-control col';
+        inputElement.setAttribute('placeholder', "Название задачи");
+
+        const buttonElement = document.createElement('button');
+        buttonElement.className = 'btn btn-primary  col mt-2';
+        buttonElement.append(document.createTextNode('Добавить'))
+
+        formInput.append(inputElement);
+        formInput.append(buttonElement);
+    }
     
+
+    const render = () => {
+        Form();
+    }
+
+    const init = () => {
+        render();
+    }
+
+    init();
 })
