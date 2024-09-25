884f7a26465a046ded13fa3e588a5c196369f4ee
Alexander Bangert <alexganbert@gmail.com>
2024-09-25 21:16:43 +0500

    render product

--- scripts/sctips.js
+++ scripts/sctips.js
@@ -1,8 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('data');
+    let tables = [
+        {
+            title: "Начало",
+            tasks: [
+                {
+                    id: 1,
+                    text: "Первая задача"
+                }
+            ]
+        },
+        {
+            title: "В прогрессе",
+            tasks: [
+                {
+                    id: 1,
+                    text: "Первая задача"
+                }
+            ]
+        },
+        {
+            title: "Готово",
+            tasks: [
+                {
+                    id: 1,
+                    text: "Первая задача"
+                }
+            ]
+        }
+    ]
+
     const Form = () => {
         const formInput = document.querySelector('#form-input');
-        console.log(formInput);
 
         const inputElement = document.createElement('input');
         inputElement.setAttribute('type', 'text');
@@ -16,10 +44,81 @@ document.addEventListener('DOMContentLoaded', () => {
         formInput.append(inputElement);
         formInput.append(buttonElement);
     }
+
+    const Tables = (tables) => {
+        const tablesElement = document.querySelector('#tables');
+
+        tables.forEach(table => {
+            tablesElement.append(Table(table));
+        });
+    }
+
+
+   
+    const Task = (task) => {
+        const taskElement = document.createElement('div');
+        taskElement.className = 'card';
+        
+        const taskBodyElement = document.createElement('div');
+        taskBodyElement.className = 'card-body';
+
+        const headerElement = document.createElement('p');
+        headerElement.innerText = task.text;
+
+        const updateButton = document.createElement('button');
+        updateButton.className = 'btn btn-warning';
+        updateButton.append(document.createTextNode('Изменить'));
+
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'btn btn-danger';
+        deleteButton.append(document.createTextNode('Удалить'));
+
+        const nextButton = document.createElement('button');
+        nextButton.className = 'btn btn-primary';
+        nextButton.append(document.createTextNode('Дальше'));
+
+        taskBodyElement.append(headerElement);
+        taskBodyElement.append(nextButton);
+        taskBodyElement.append(deleteButton);
+        taskBodyElement.append(updateButton);
+
+
+        taskElement.append(taskBodyElement);
+
+        return taskElement;
+    }
     
+
+
+    const Table = (table) => {
+
+        const tableElement = document.createElement('div');
+        tableElement.className = "list_item card mx-2";
+
+        const tableHeaderElement = document.createElement('div');
+        tableHeaderElement.className = 'card-header';
+        tableHeaderElement.append(document.createTextNode(table.title));
+
+        const tableBodyElement = document.createElement('div');
+        tableBodyElement.className = 'card-body';
+
+        table.tasks.forEach(task => {
+            tableBodyElement.append(Task(task));
+        })
+        
+        tableElement.append(tableHeaderElement);
+        tableElement.append(tableBodyElement);
+        
+
+        return tableElement;
+
+    }
+
 
     const render = () => {
         Form();
+        Tables(tables);
+
     }
 
     const init = () => {
