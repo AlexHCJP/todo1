f5dd12d31a89437eca3812ce6e26585642a7b922
Alexander Bangert <alexganbert@gmail.com>
2024-10-02 17:27:34 +0500

    refactoring

--- scripts/sctips.js
+++ scripts/sctips.js
@@ -1,72 +1,70 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let tables = [
-        {
-            title: "Начало",
-            tasks: [
-                {
-                    id: 1,
-                    text: "Первая задача"
-                }
-            ]
-        },
-        {
-            title: "В прогрессе",
-            tasks: [
-                {
-                    id: 2,
-                    text: "Первая задача"
-                }
-            ]
-        },
-        {
-            title: "Готово",
-            tasks: [
-                {
-                    id: 3,
-                    text: "Первая задача"
-                }
-            ]
-        }
-    ]
+const initialData = [
+    {
+        title: "Начало",
+        tasks: []
+    },
+    {
+        title: "В прогрессе",
+        tasks: []
+    },
+    {
+        title: "Готово",
+        tasks: []
+    }
+]
 
+document.addEventListener('DOMContentLoaded', () => {
+    // Таблицы заданий
+    let tables;
+    // id задачи, которая будет изменяться
     let updatedId;
 
+    // Изменение задание
+    const putTaskAction = (text) => {
+        tables = tables.map(table => {
+            table.tasks = table.tasks.map(task => {
+                if(task.id === updatedId) {
+                    task.text = text;
+                }
+                return task;
+            });
+            return table;
+        });
+        updatedId = null;
 
-    const onCreateTask = (form) => (ev) => {
+        return tables;
+    }
+
+    // Создание задания
+    const createTaskAction = (text) => {
+        tables[0].tasks.push({
+            id: Date.now(),
+            text
+        });
+        return tables;
+    }
+
+    const onFormAction = (form) => (ev) => {
+        // Убираем обычное поведение
         ev.preventDefault();
 
+        // Получаем значение из формы
         const title = new FormData(form).get('title');
         if(!title) return; 
 
         if(updatedId) {
-            tables = tables.map((table) => {
-                table.tasks = table.tasks.map((task) => {
-                    if(updatedId == task.id) {
-                        task.text = title;
-                    } 
-                    return task;
-                })
-                return table;
-            })
+            putTaskAction(title);
         } else {
-            // if(title.length <= 0) return;
-
-            tables[0].tasks.push({
-                id: Date.now(),
-                text: title,
-            });
+            createTaskAction(title);
         }
 
         updatedId = null;
-        render();
-
-        
+        return render();    
     }
-
 
     const onUpdateTask = (task) => (ev) => {
         updatedId = task.id;
-        render();
+        return render();
     }  
 
     const onDeleteTask = (task) => (ev) => {
@@ -74,56 +72,41 @@ document.addEventListener('DOMContentLoaded', () => {
             table.tasks = table.tasks.filter((value) => value.id != task.id);
             return table;
         })
-        render();
+        return render();
     }
 
-    const onNextTask = (nextTask) => (ev) => {
-        let nextIndex;
-        tables = tables.map((table, tableIndex) => {
-            console.log('index', nextIndex, tableIndex);
-            if(nextIndex == tableIndex) {
-                table.tasks.push(nextTask);
-            } else {
-                table.tasks = table.tasks.filter((value, index) => {
-                    const check = value.id != nextTask.id;
-                    console.log('Check', nextIndex, tableIndex);
-                    if(!check) {
-                        nextIndex = tableIndex + 1;
-                    }
-                    return check;
-                });
-    
+    const onNextTask = (task) => (ev) => {
+        const taskId = task.id;
+
+        let currentIndex = tables.findIndex(table => table.tasks.find(task => task.id === taskId));
+        let nextIndex = currentIndex + 1;
+        tables = tables.map((table, index) => {
+            if(currentIndex === tables.length - 1) return table;
+            if(currentIndex === index) {
+                table.tasks = table.tasks.filter(task => task.id !== taskId)
             }
-            
+            if(nextIndex === index) {
+                table.tasks.push(task)
+            };
             return table;
         });
         render();
+
+        return tables;
     }
 
     const Form = () => {
         const formInput = document.querySelector('#form-input');
         formInput.innerHTML = '';
-        formInput.addEventListener('submit', onCreateTask(formInput))
-
-
-
-        const task = tables.reduce((prevValue, value) => {
-            if(prevValue) return prevValue;
-            const task = value.tasks.find((element, index, arrays) => {
-                console.log(element.id, updatedId);
-                return element.id == updatedId;
-            });
-            return task;
-        }, null);
-
-        
-        
+        formInput.addEventListener('submit', onFormAction(formInput))
 
         const inputElement = document.createElement('input');
         inputElement.setAttribute('name', 'title');
         inputElement.setAttribute('type', 'text');
         inputElement.className = 'form-control col';
         inputElement.setAttribute('placeholder', "Название задачи");
+
+        let task = getTaskById(updatedId);
         if(task) {
             inputElement.setAttribute('value', task.text);
         }
@@ -131,10 +114,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const buttonElement = document.createElement('input');
         buttonElement.setAttribute('type', 'submit');
         buttonElement.className = 'btn btn-primary  col mt-2';
-        buttonElement.append(document.createTextNode('Добавить'))
+        buttonElement.setAttribute('value', task? 'Update' : 'Create');
 
         formInput.append(inputElement);
         formInput.append(buttonElement);
+
+        return formInput;
     }
 
     const Tables = (tables) => {
@@ -144,9 +129,10 @@ document.addEventListener('DOMContentLoaded', () => {
         tables.forEach(table => {
             tablesElement.append(Table(table));
         });
+
+        return tablesElement;
     }
 
-   
     const Task = (task) => {
         const taskElement = document.createElement('div');
         taskElement.className = 'card';
@@ -164,22 +150,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const deleteButton = document.createElement('button');
         deleteButton.className = 'btn btn-danger';
-        deleteButton.append(document.createTextNode('Удалить'));
         deleteButton.addEventListener('click', onDeleteTask(task));
 
-        const nextButton = document.createElement('button');
-        nextButton.className = 'btn btn-primary';
-        nextButton.append(document.createTextNode('Дальше'));
-        nextButton.addEventListener('click', onNextTask(task));
-        
+        const icon = document.createElement('img');
+        icon.src = 'assets/x.svg';
+        deleteButton.append(icon);
 
         taskBodyElement.append(headerElement);
-        taskBodyElement.append(nextButton);
         taskBodyElement.append(deleteButton);
         taskBodyElement.append(updateButton);
-
-
         taskElement.append(taskBodyElement);
+
+        nextTitleTable = tables[tables.findIndex(table => table.tasks.find(el => el.id === task.id)) + 1]?.title ?? null;
+        if(nextTitleTable) {
+            let nextButton = document.createElement('button');
+            nextButton.classList.add('btn', 'btn-primary');
+            nextButton.appendChild(document.createTextNode(nextTitleTable));
+            nextButton.addEventListener('click', onNextTask(task));
+            taskBodyElement.appendChild(nextButton);
+            taskBodyElement.append(nextButton);
+
+        }
+        
+
+        
 
         return taskElement;
     }
@@ -203,21 +197,37 @@ document.addEventListener('DOMContentLoaded', () => {
         tableElement.append(tableHeaderElement);
         tableElement.append(tableBodyElement);
         
-
         return tableElement;
 
     }
 
-
     const render = () => {
-        console.log('render');
+        setStorageData();
         Form();
         Tables(tables);
+        
+        return true;
     }
 
     const init = () => {
+        getStorageData();
         render();
+
+        return true;
     }
+
+    const getStorageData = () => tables = JSON.parse(localStorage.getItem('tables')) ?? initialData;
+
+    const setStorageData = () => localStorage.setItem('tables', JSON.stringify(tables))
+
+    // Получаем Задание по Id
+    const getTaskById = (id) => tables.reduce((acc, table) => {
+        const task = table.tasks.find(task => task.id === id);
+        if(task) {
+            acc = task;
+        }
+        return acc;
+    }, null);
 
     init();
 })
